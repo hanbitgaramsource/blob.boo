460af42b7c2ced1c37e26181866fdaa15ee456f7
한빛가람(ハンビッガラム) <webmaster@hanb.jp>
2024-01-08 10:40:27 +0900

    blahblah

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import blob from './assets/blob.svg';
 import './App.css';
+import {useState} from "react";
+import jQuery from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [clickTimes, setClickTimes] = useState(0);
+    const blobClickEvent = () => {
+        setClickTimes(clickTimes+1);
+        if(jQuery('.blobDiv').hasClass('on')){
+
+        }else{
+            jQuery('.blobDiv').addClass('on');
+            setTimeout(()=>{
+                jQuery('.blobDiv').removeClass('on');
+            }, 200);
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className={"blobDiv"} onClick={()=>{blobClickEvent()}}><img src={blob} className="Blob-Image" alt="blob" tabIndex={0} /></div>
+                <p>
+                    You clicked the blob {clickTimes} times!
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
